a4ee53d9310227b44d66cac600c05c4ba41e550f
rizal-mujahiddan69 <ganteng3rizal@apps.ipb.ac.id>
2022-04-05 23:41:59 +0700

    add main

--- main.r
+++ main.r
@@ -1,3 +1,15 @@
-data <- read.csv("dataset.csv")
+library(tidyverse)
 
-summary(data)
+data <- read.csv("dataset.csv")
+ukuran_data <- dim(data)
+
+# Check column if have NA's
+kolom_NA <- data.frame(as.list(colSums(is.na(data))))
+kolom_NA <- as.data.frame(t(kolom_NA))
+colnames(kolom_NA) <- c("freq")
+
+# pipelining https://raw.githubusercontent.com/rstudio/cheatsheets/main/data-transformation.pdf
+kolom_NA <- kolom_NA %>% 
+  filter(freq > 0) %>% 
+  mutate(percen_freq = freq / ukuran_data[1] * 100)
+print(kolom_NA)
