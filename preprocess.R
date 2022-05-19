a70caec826ed1278ce8ec70bc1d7c5003a7c0abe
rizal-mujahiddan69 <ganteng3rizal@apps.ipb.ac.id>
2022-05-19 16:28:03 +0700

    update describe

--- preprocess.R
+++ preprocess.R
@@ -86,10 +86,9 @@ for(kolomku in kolom_NA_cat_factor){
 data <- data %>% select(-c(encounter_id,patient_id,
                            hospital_id,icu_id))
 
-kol_char <- names(which(lapply(data,class)=="character"))
-
 
 # Imputation and Discrezation Dataset
+kol_char <- names(which(lapply(data,class)=="character"))
 
 for(kol in kol_char){
   mode_impute <- names(sort(-table(data[[kol]])))[1]
@@ -103,37 +102,42 @@ mutate(hospital_death = case_when(hospital_death == 0 ~ 'Survived',
                                   hospital_death == 1 ~ 'Death'))
 
 data$hospital_death <- factor(data$hospital_death,levels = c("Survived","Death"),labels = c("Survived","Death"))
-N_min <- min(unname(table(data$hospital_death)))
-data <- ovun.sample(hospital_death ~ ., data=data,N=2*N_min ,seed=1234)$data
+
+
+
+
+
+# N_min <- min(unname(table(data$hospital_death)))
+# data <- ovun.sample(hospital_death ~ ., data=data,N=2*N_min ,seed=1234)$data
 
 # Splitting Data
-set.seed(1234)
-ind <- sample(2, nrow(data), replace = T, prob = c(0.8, 0.2))
-trainData <- data[ind == 1,]
-testData <- data[ind == 2,]
+# set.seed(1234)
+# ind <- sample(2, nrow(data), replace = T, prob = c(0.8, 0.2))
+# trainData <- data[ind == 1,]
+# testData <- data[ind == 2,]
 
 # feature selection 
-FDA_Model <- train(hospital_death ~ . , data = trainData, method='fda')
-FDA_Imp <- varImp(FDA_Model)
-FDA_Model_Imp <- FDA_Imp$importance
-FDA_Model_Imp <- FDA_Model_Imp %>% filter(Overall>0)
-feature_selected <- rownames(FDA_Model_Imp)
+# FDA_Model <- train(hospital_death ~ . , data = trainData, method='fda')
+# FDA_Imp <- varImp(FDA_Model)
+# FDA_Model_Imp <- FDA_Imp$importance
+# FDA_Model_Imp <- FDA_Model_Imp %>% filter(Overall>0)
+# feature_selected <- rownames(FDA_Model_Imp)
 # feature_selected[8] <- substr(feature_selected[8],1,nchar(feature_selected[8])-1)
 # feature_selected[9] <- substr(feature_selected[9],1,nchar(feature_selected[9])-1)
-feature_selected <- c(feature_selected,"hospital_death")
-feature_selected <- gsub("`","",feature_selected)
-feature_selected <- gsub("^(.*)\\d$","\\1",feature_selected)
-feature_selected <- gsub("^(icu_[a-z|_]*)[A-Z].*$","\\1",feature_selected)
-feature_selected <- gsub("^(apache_[0-9|a-z|_]*)[A-Z].*$","\\1",feature_selected)
+# feature_selected <- c(feature_selected,"hospital_death")
+# feature_selected <- gsub("`","",feature_selected)
+# feature_selected <- gsub("^(.*)\\d$","\\1",feature_selected)
+# feature_selected <- gsub("^(icu_[a-z|_]*)[A-Z].*$","\\1",feature_selected)
+# feature_selected <- gsub("^(apache_[0-9|a-z|_]*)[A-Z].*$","\\1",feature_selected)
 
 # print(feature_selected)
 # setdiff(feature_selected,names(data))
 # names(data)[grep("^apache_",names(data))]
 
-trainData <- trainData[,feature_selected]
-testData <- testData[,feature_selected]
-
-current_1 <- Sys.time()
-deltatime_pre <- current_1 - current_0
-print("Preprocessing Time :")
-print(deltatime_pre)
+# trainData <- trainData[,feature_selected]
+# testData <- testData[,feature_selected]
+# 
+# current_1 <- Sys.time()
+# deltatime_pre <- current_1 - current_0
+# print("Preprocessing Time :")
+# print(deltatime_pre)
